6b2bc724739209e52a63910258b29052ae1af083
sjiyeng <81696291+sjiyeng@users.noreply.github.com>
2023-01-14 18:53:12 -0500

    Updated Navx vendordep

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -38,7 +38,6 @@ public final class Constants {
         new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
     // Angular offsets of the modules relative to the chassis in radians
-    //TODO: FIND THE ACTUAL ONES
     public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
     public static final double kFrontRightChassisAngularOffset = 0;
     public static final double kBackLeftChassisAngularOffset = Math.PI;
