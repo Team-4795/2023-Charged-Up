bd4f6cb0c549e1bbfdc102f1631e6dc2ed64e05c
natasha <nlueckingrivasplata@students.chccs.k12.nc.us>
2023-01-26 12:50:21 -0500

    just updating a line so i can open the code

--- src/main/java/frc/robot/subsystems/endEffectorIntake.java
+++ src/main/java/frc/robot/subsystems/endEffectorIntake.java
@@ -4,7 +4,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import thisporfavor
 
 public class endEffectorIntake extends SubsystemBase {
     
