4309de57ee525d37625d94e3af917eaf9cb6e064
natasha <nlueckingrivasplata@students.chccs.k12.nc.us>
2023-01-26 17:12:52 -0500

    fixed issues w/ variables

--- src/main/java/frc/robot/subsystems/endEffectorIntake.java
+++ src/main/java/frc/robot/subsystems/endEffectorIntake.java
@@ -3,16 +3,13 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class endEffectorIntake extends SubsystemBase {
     
     private final CANSparkMax intakeMotor;
-    private final DigitalInput cargoOne, cargoTwo, cargoThree;
     
-
     public endEffectorIntake() {
         int Intake = Constants.kIntake;
         intakeMotor = new CANSparkMax(Intake, MotorType.kBrushed);
@@ -20,13 +17,15 @@ public class endEffectorIntake extends SubsystemBase {
         //im not sure tho
         //it just told me to put a device id so i put that
 
-    public void setMotorSpeed(double speed) {
+    }
+    public void setMotorSpeed(double speed)
+    {
         intakeMotor.set(speed);
     }
 
-    public void stopIntake(double speed) {
+    public void stopIntake(double speed) 
+    {
         intakeMotor.set(speed = 0);  
     }
-
 
 }
