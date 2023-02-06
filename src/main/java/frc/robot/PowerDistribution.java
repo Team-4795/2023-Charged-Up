c2f4ba8ecf46f42f7625819037987e946620f517
Darya483 <56742538+Darya483@users.noreply.github.com>
2023-02-06 16:20:08 -0500

    pulls pDP temperature as per #4

--- src/main/java/frc/robot/PowerDistribution.java
+++ src/main/java/frc/robot/PowerDistribution.java
@@ -3,12 +3,20 @@ package frc.robot;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 
 public class PowerDistribution {
+    public PowerDistribution(int i, ModuleType krev) {
+    }
+    private static final edu.wpi.first.wpilibj.PowerDistribution m_pdp = null;
     PowerDistribution PD = new PowerDistribution(1, ModuleType.kRev);
     // Get the voltage going into the PDP, in increments of 0.05 Volts (can be used for brownout detection in DS)
     double voltage = m_pdp.getVoltage();
     SmartDashboard.putNumber("Voltage", voltage);
-    void 
+
+
+    
+    // Retrieves PDP temp, in C.
+    double temperatureCelsius = m_pdp.getTemperature();
 }
 
