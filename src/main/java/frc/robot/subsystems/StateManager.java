d38bd28647730252b381a6949d5d7223e4b96221
SuperWinner50 <millhouseaa@gmail.com>
2023-02-07 16:56:55 -0500

    Update constants

--- src/main/java/frc/robot/subsystems/StateManager.java
+++ src/main/java/frc/robot/subsystems/StateManager.java
@@ -105,8 +105,8 @@ enum State {
             case LowScore: result = new Setpoints(0.907, 0.0, 0.0); break;
             case MidScore: result = new Setpoints(0.772, 0.0, 0.0); break;
             case HighScoreCube: result = new Setpoints(0.75, 0.0, 0.0); break;
-            case StowInFrame: break;
-            case StowLow: break;            
+            case StowInFrame: new Setpoints(0.16, 0.0, 0.0); break;
+            case StowLow: new Setpoints(0.96, 0.0, 0.0); break;
         }
 
         return Optional.ofNullable(result);
@@ -122,8 +122,8 @@ enum State {
             case LowScore: result = new Setpoints(0.907, 0.0, 0.0); break;
             case MidScore: result = new Setpoints(0.714, 0.0, 0.0); break;
             case HighScoreCube: break;
-            case StowInFrame: break;
-            case StowLow: break;     
+            case StowInFrame: new Setpoints(0.16, 0.0, 0.0); break;
+            case StowLow: new Setpoints(0.96, 0.0, 0.0); break;
         }
 
         return Optional.ofNullable(result);
