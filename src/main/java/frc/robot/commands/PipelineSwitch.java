84be81e1042fa9636a741b30644d36004f5a0c1e
natasha <nlueckingrivasplata@students.chccs.k12.nc.us>
2023-02-15 07:02:43 -0500

    pipeline switch command started

---
+++ src/main/java/frc/robot/commands/PipelineSwitch.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.vision.VisionPipeline;
+import frc.robot.subsystems.Vision;
+import frc.robot.commands.Align;
+import frc.robot.commands.TapeAlign;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.PhotonCamera;
+
+public class PipelineSwitch extends CommandBase {
+  private final PhotonCamera camera;
+  private final Vision vision;
+
+  public void SwitchToAlign(int index) {
+    camera.setPipelineIndex(1);
+  }
+
+  public void SwitchToTape(int index) {
+    camera.setPipelineIndex(0);
+  }
+  
+  /** Creates a new PipelineSwitch. */
+  public PipelineSwitch(PhotonCamera camera, Vision vision) {
+    this.camera = camera;
+    this.vision = vision;
+
+    addRequirements(vision);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    camera.setPipelineIndex(1);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
