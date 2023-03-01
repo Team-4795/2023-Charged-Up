c13082bbf2352e62ba39676e35d3e30b1634ff7e
sjiyeng <81696291+sjiyeng@users.noreply.github.com>
2023-03-01 12:18:40 -0500

    testing from 2/28

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.common.hardware.VisionLEDMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import frc.robot.Constants;
 import frc.robot.Constants.VisionConstants;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -24,6 +28,7 @@ public class Vision extends SubsystemBase{
 
    
     private double targetAngle = VisionConstants.kTargetAngle;
+    private double range;
     double forwardSpeed;
     double x_pitch = VisionConstants.kX_Pitch;
 
@@ -73,30 +78,43 @@ public class Vision extends SubsystemBase{
       NetworkTableEntry tx = table.getEntry("tx");
       NetworkTableEntry ty = table.getEntry("ty");
       NetworkTableEntry ta = table.getEntry("ta");
-      
+      NetworkTableEntry tv = table.getEntry("tv");
+
       NetworkTableValue tv_value = table.getValue("tv");
       NetworkTableValue tx_value = table.getValue("tx");
       double area;
       double y;
       double x;
       NetworkTableEntry botPose = NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose"); //AHHHHHHH
-      
-      float tv = tv_value.getFloat();
-      
+            
     @Override
     public void periodic() {
             //read values periodically
       double x = tx.getDouble(0.0);
       double y = ty.getDouble(0.0);
       double area = ta.getDouble(0.0);
+
       double targetAngle = x; //need to find out if tx is angle or distance
       float steering_adjust = 0.0f;
 
-        if (tv == 0.0) {
+        if (tv.getDouble(0.0) == 0.0) {
             hasTargets = false;
+            // range = -1;
           } else {
+            // range = PhotonUtils.calculateDistanceToTargetMeters(
+            //         CameraHeight,
+            //         TargetHeight,
+            //         cameraPitchRadians ,
+            //         Units.degreesToRadians(result.getBestTarget().getPitch()));
             hasTargets = true;
     }
+
+    // SmartDashboard.putNumber("Distance between target", range);   
+    SmartDashboard.putBoolean("Has target", hasTargets);   
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+
           /* 
     @Override
     public void initSendable(SendableBuilder builder) {
