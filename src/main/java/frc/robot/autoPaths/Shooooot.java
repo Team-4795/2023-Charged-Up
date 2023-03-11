bb907bc7c9f8719d838156b6b0caab0f6421bf26
paulrosu11 <paulrosu11@gmail.com>
2023-03-10 21:39:09 -0500

    Shoot from charging station

--- src/main/java/frc/robot/autoPaths/Shooooot.java
+++ src/main/java/frc/robot/autoPaths/Shooooot.java
@@ -9,21 +9,22 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.AutoSelector;
 import frc.robot.StateManager;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.EndEffectorIntake;
-import frc.robot.subsystems.LiftArm;
-import frc.robot.subsystems.Vision;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Commands.AutoBalanceOld;
 import frc.robot.Commands.DriveCommandOld;
 import frc.robot.Commands.TapeAlign;
 import frc.robot.Constants.AutoConstants;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.EndEffectorIntake;
+import frc.robot.subsystems.LiftArm;
+import frc.robot.subsystems.Vision;
 
 public class Shooooot extends SequentialCommandGroup {
 
@@ -71,8 +72,30 @@ public class Shooooot extends SequentialCommandGroup {
                 drivebase.followTrajectoryCommand(AutoBalance),
                 m_autoSelector.stow(m_intake, m_manager, m_arm)),
 
-            new DriveCommandOld(drivebase, -AutoConstants.driveBalanceSpeed, AutoConstants.driveAngleThreshold,
-                AutoConstants.checkDuration).withTimeout(AutoConstants.overrideDuration),
-            new AutoBalanceOld(drivebase, AutoConstants.angularVelocityErrorThreshold)));
+
+            new ParallelCommandGroup(
+                new SequentialCommandGroup(
+                    new DriveCommandOld(drivebase, -AutoConstants.driveBalanceSpeed, AutoConstants.driveAngleThreshold,
+                        AutoConstants.checkDuration).withTimeout(AutoConstants.overrideDuration),
+                    new AutoBalanceOld(drivebase, AutoConstants.angularVelocityErrorThreshold))),
+                    
+            new SequentialCommandGroup(
+                new InstantCommand(() -> m_intake.setOverrideStoring(true)),
+                new InstantCommand(m_manager::pickCube),
+                new ParallelRaceGroup(
+                    new SequentialCommandGroup(
+                        new InstantCommand(() -> m_arm.setPosition(.35),m_arm),
+                        new WaitUntilCommand(m_arm::atSetpoint),
+                        new InstantCommand(m_intake::retract, m_intake)),
+                    new SequentialCommandGroup(
+                        new InstantCommand(() -> {
+                          m_vision.switchToTag();
+                        }),
+                        new TapeAlign(
+                            drivebase, m_vision,
+                            () -> AutoConstants.VisionXspeed, () -> AutoConstants.VisionYspeed))),
+                new RunCommand(() -> m_intake.intake(-1),m_intake),withTimeout(.2),
+                new InstantCommand(() -> m_intake.setOverrideStoring(false)))));
+
   }
 }
