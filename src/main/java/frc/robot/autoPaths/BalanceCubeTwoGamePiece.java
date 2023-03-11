502b494f5983e3c45c6d75e7064a5d3f6ac6cf67
paulrosu11 <paulrosu11@gmail.com>
2023-03-10 20:32:53 -0500

    Error fix

--- src/main/java/frc/robot/autoPaths/BalanceCubeTwoGamePiece.java
+++ src/main/java/frc/robot/autoPaths/BalanceCubeTwoGamePiece.java
@@ -9,30 +9,30 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.AutoSelector;
-import frc.robot.Constants.AutoConstants;
 import frc.robot.StateManager;
-import frc.robot.Commands.TapeAlign;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.EndEffectorIntake;
 import frc.robot.subsystems.LiftArm;
 import frc.robot.subsystems.Vision;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Commands.AutoBalanceOld;
+import frc.robot.Commands.DriveCommandOld;
+import frc.robot.Commands.TapeAlign;
+import frc.robot.Constants.AutoConstants;
 
+public class BalanceCubeTwoGamePiece extends SequentialCommandGroup {
 
-
-public class FreeCubeTwoGamePiece extends SequentialCommandGroup {
-
-public FreeCubeTwoGamePiece(DriveSubsystem drivebase, EndEffectorIntake m_intake, LiftArm m_arm, Field2d m_field,
+public BalanceCubeTwoGamePiece(DriveSubsystem drivebase, EndEffectorIntake m_intake, LiftArm m_arm, Field2d m_field,
       StateManager m_manager, Vision m_vision, AutoSelector m_autoSelector) {
 
   PathPlannerTrajectory CubeTwoGamePiece1 = PathPlanner.loadPath("Free Cube 2 Game Piece 1", new PathConstraints(1, 2));
   PathPlannerTrajectory CubeTwoGamePiece2 = PathPlanner.loadPath("Free Cube 2 Game Piece 2", new PathConstraints(1, 2));
-
+  PathPlannerTrajectory AutoBalance = PathPlanner.loadPath("Auto Balance Left", new PathConstraints(3, 3));
     // Add option of Vision based two game peice split into parts with commands Cube
   addCommands(
     new SequentialCommandGroup(
@@ -65,7 +65,14 @@ public FreeCubeTwoGamePiece(DriveSubsystem drivebase, EndEffectorIntake m_intake
         // Run outake for 1 second to scorenew RunCommand(m_intake::outtake,
         // m_intake).withTimeout(1.0),
         new InstantCommand(m_intake::retract, m_intake),
-        new InstantCommand(() -> m_intake.setOverrideStoring(false)))
-       );
+        new InstantCommand(() -> m_intake.setOverrideStoring(false)),
+
+        new ParallelCommandGroup(
+           drivebase.followTrajectoryCommand(AutoBalance),
+            m_autoSelector.stow(m_intake, m_manager, m_arm)),
+
+        new DriveCommandOld(drivebase, -AutoConstants.driveBalanceSpeed, AutoConstants.driveAngleThreshold,
+        AutoConstants.checkDuration).withTimeout(AutoConstants.overrideDuration),
+        new AutoBalanceOld(drivebase, AutoConstants.angularVelocityErrorThreshold)));
       }
-    }
+}
