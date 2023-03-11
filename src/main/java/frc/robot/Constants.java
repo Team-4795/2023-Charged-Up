55c0f3914b0230dade1aa93f5494427d8075e1ed
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-03-11 16:29:26 -0500

    Important changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -233,7 +233,7 @@ public final class Constants {
   }
 
   public static final class TapeAlignConstants {
-    public static final double kP = .011;
+    public static final double kP = .015;
     public static final double kI = 0;
     public static final double kD = 0;
     public static final double kAngularThreshold = 2;
@@ -323,7 +323,7 @@ public final class Constants {
     public static final double kTargetHeight = 0;
     public static final double kCameraPitchRadians = -5;
     public static final double kX_Pitch = 0;
-    public static final double kTargetAngle = 0;
+    public static final double kTargetAngle =- 4;
 
     /**
      * Physical location of the camera on the robot, relative to the center of the robot.
--- src/main/java/frc/robot/autoPaths/FreeCubeTwoGamePiece.java
+++ src/main/java/frc/robot/autoPaths/FreeCubeTwoGamePiece.java
@@ -9,16 +9,10 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.AutoSelector;
-import frc.robot.Constants.AutoConstants;
 import frc.robot.StateManager;
-import frc.robot.Commands.TapeAlign;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.EndEffectorIntake;
 import frc.robot.subsystems.LiftArm;
@@ -27,8 +21,7 @@ import frc.robot.subsystems.Wrist;
 
 public class FreeCubeTwoGamePiece extends SequentialCommandGroup {
 
-  public FreeCubeTwoGamePiece(DriveSubsystem drivebase, EndEffectorIntake m_intake, LiftArm m_arm, Field2d m_field,
-      StateManager m_manager, Vision m_vision, AutoSelector m_autoSelector, Wrist wrist ) {
+  public FreeCubeTwoGamePiece(DriveSubsystem drivebase, EndEffectorIntake m_intake, LiftArm m_arm, Field2d m_field, StateManager m_manager, Vision m_vision, AutoSelector m_autoSelector, Wrist wrist ) {
 
     PathPlannerTrajectory CubeTwoGamePiece1 = PathPlanner.loadPath("Free Cube 2 Game Piece 1",
         new PathConstraints(2, 3));
@@ -44,8 +37,8 @@ public class FreeCubeTwoGamePiece extends SequentialCommandGroup {
             new ParallelCommandGroup(
                 drivebase.followTrajectoryCommand(CubeTwoGamePiece1),
                 m_autoSelector.intake("cube", m_intake, m_manager, m_arm, wrist)),
-
+            drivebase.followTrajectoryCommand(CubeTwoGamePiece2),
             m_autoSelector.score("cube", "mid", m_intake, m_manager, m_arm, drivebase, m_vision, wrist)
-          ));
+    ));
   }
 }
