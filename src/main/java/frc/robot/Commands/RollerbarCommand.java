4d76ccb233c229d800da9db68bdd523fc4ea34d3
Alan Liu <liualan998@gmail.com>
2023-03-27 12:00:59 -0400

    Fixed Auto Rollerbar Command

--- src/main/java/frc/robot/Commands/RollerbarCommand.java
+++ src/main/java/frc/robot/Commands/RollerbarCommand.java
@@ -4,26 +4,49 @@
 
 package frc.robot.Commands;
 
+import java.util.Optional;
+
+import javax.swing.plaf.basic.BasicBorders.RolloverButtonBorder;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.StateManager;
+import frc.robot.Constants.RollerbarConstants;
 import frc.robot.subsystems.*;
 
 public class RollerbarCommand extends CommandBase {
   Rollerbar rollerbar;
   LiftArm arm;
+  StateManager manager;
+  boolean targetExtend;
+  Optional<Double> previousSetpoint;
+  boolean returned;
 
-  public RollerbarCommand(Rollerbar rollerbar, LiftArm arm) {
+  public RollerbarCommand(Rollerbar rollerbar, LiftArm arm, StateManager manager, boolean extend) {
     this.rollerbar = rollerbar;
     this.arm = arm;
-
+    targetExtend = extend;
+    this.manager = manager;
+    returned = false;
     addRequirements(rollerbar, arm);
   }
 
   @Override
-  public void initialize() {}
+  public void initialize() {
+    previousSetpoint = manager.getArmSetpoint();
+    rollerbar.setTarget(targetExtend);
+    if(arm.getPosition() < RollerbarConstants.kArmBoundary){
+      arm.setTargetPosition(RollerbarConstants.kArmBoundary + 0.05);
+    }
+  }
 
   @Override
   public void execute() {
+    arm.runAutomatic();
     rollerbar.tryMove(arm.getPosition());
+    if(arm.getPosition() > RollerbarConstants.kArmBoundary + 0.02 && !returned){
+      previousSetpoint.ifPresent(arm::setTargetPosition);
+      returned = true;
+    }
   }
 
   @Override
@@ -31,6 +54,6 @@ public class RollerbarCommand extends CommandBase {
 
   @Override
   public boolean isFinished() {
-    return false;
+    return (rollerbar.isExtended() == targetExtend && arm.atSetpoint());
   }
 }
