f98e24e7a54656bd3c65746d29d21688aeaed1b8
Alan Liu <liualan998@gmail.com>
2023-03-31 09:25:21 -0400

    Readded Pathplanner Paths
    oops...

--- src/main/java/frc/robot/autoPaths/AutoTest.java
+++ src/main/java/frc/robot/autoPaths/AutoTest.java
@@ -6,14 +6,14 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.AutoSelector;
-import frc.robot.subsystems.DriveSubsystem;
 
 public class AutoTest extends SequentialCommandGroup{
     public AutoTest(AutoSelector selector){
         PathPlannerTrajectory test = PathPlanner.loadPath("Testing Path", new PathConstraints(2, 2));
         addCommands(new SequentialCommandGroup(
             selector.autoStartUp(test, false),
-            selector.stowTrajectory(test)
+            selector.scoreTrajectory("cube", "low", false, test),
+            selector.outtake(0.5)
         ));
     }
 }
