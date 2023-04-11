5699a7efc801303448ea7eefa9c26ab66bcf07ea
Alan Liu <liualan998@gmail.com>
2023-04-11 09:29:20 -0400

    World's PathPlanner Cleanup

--- src/main/java/frc/robot/autoPaths/Free3HybridMHM.java
+++ src/main/java/frc/robot/autoPaths/Free3HybridMHM.java
@@ -9,9 +9,9 @@ import frc.robot.AutoSelector;
 
 public class Free3HybridMHM extends SequentialCommandGroup{
     public Free3HybridMHM(AutoSelector selector){
-        PathPlannerTrajectory intakeGP1 = PathPlanner.loadPath("Intake Free N1 GP1 Blue", new PathConstraints(4, 3.5));
+        PathPlannerTrajectory intakeGP1 = PathPlanner.loadPath("Intake Free N1 GP1", new PathConstraints(4, 3.5));
         PathPlannerTrajectory scoreGP1 = PathPlanner.loadPath("Score Free N2 GP1", new PathConstraints(4, 3.5));
-        PathPlannerTrajectory intakeGP2 = PathPlanner.loadPath("Intake Free N2 GP2 Blue", new PathConstraints(4, 3.5));
+        PathPlannerTrajectory intakeGP2 = PathPlanner.loadPath("Intake Free N2 GP2", new PathConstraints(4, 3.5));
         PathPlannerTrajectory scoreGP2 = PathPlanner.loadPath("Score Free N2 GP2", new PathConstraints(4, 3.5));
         addCommands(new SequentialCommandGroup(
             selector.autoStartUp(intakeGP1, false),
