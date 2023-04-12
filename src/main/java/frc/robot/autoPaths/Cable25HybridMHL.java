af5866d64820408b87aa89f5f97fbfb24cdca1c3
Alan Liu <liualan998@gmail.com>
2023-04-11 21:15:27 -0400

    Minor Cable 2.5 Fixes

--- src/main/java/frc/robot/autoPaths/Cable25HybridMHL.java
+++ src/main/java/frc/robot/autoPaths/Cable25HybridMHL.java
@@ -13,7 +13,9 @@ public class Cable25HybridMHL extends SequentialCommandGroup{
         PathPlannerTrajectory scoreGP4 = PathPlanner.loadPath("Score Cable N2 GP4", new PathConstraints(4, 3.5));
         PathPlannerTrajectory intakeGP3 = PathPlanner.loadPath("Intake Cable N2 GP3", new PathConstraints(4, 3.5));
         addCommands( new SequentialCommandGroup(
+            selector.autoStartUp(intakeGP4, false),
             selector.score("cone", "mid", false),
+            selector.outtake(0.3),
             selector.intakeTrajectory("cube", true, intakeGP4),
             selector.scoreTrajectory("cube", "high", false, scoreGP4),
             selector.outtake(0.3),
